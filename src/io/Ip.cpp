0d291d20e90f32b06e18aea9a8b8488e81591b0b
SavaLione <monologuesplus@gmail.com>
2017-11-12 23:40:10 +0500

    Update Ip.cpp

--- src/io/Ip.cpp
+++ src/io/Ip.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 /** Парсинг ip адресов
-	\param[in] ch_return Массив char с ip адресом
+	\param[in] ch_ip_addr Массив char с ip адресом
     \param[out] uc_arr_return Возвращяет массив типа unsigned char
 	\return Массив из 4 переменных типа unsigned char
     \example v_convert_ip.cpp
@@ -17,16 +17,24 @@ using namespace std;
 void v_convert_ip(char *ch_ip_addr, unsigned char* uc_arr_return) {
 	// Массив unsigned char. Заполнен пустым значением. Разделён на 4 блока. Нужен для хранения байт ip адресов
     unsigned char uc_arr[4] = {0};
+	// size_t переменная. Плюс в портируемости приложений (32 => 64) 
 	size_t index = 0;
     while (*ch_ip_addr) {
+		/*
+			Функция isdigit проверяет аргумент, является ли он десятичной цифрой. Приведение типа и указатель массива char с ip адресом.
+			Если элемент массива - цифра, то элемент массива index умножаем на 10, к элементу массива index прибавляем текущий элемент масива ch_ip_addr.
+			Если элемент массива - не цифра, то переходим к следующему блоку ip адреса
+		*/
         if (isdigit((unsigned char)*ch_ip_addr)) {
             uc_arr[index] *= 10;
             uc_arr[index] += *ch_ip_addr - '0';
         } else {
             index++;
         }
+		// Переходим к следующему элементу массива ch_ip_addr.
         ch_ip_addr++;
     }
+	// После того, как элементы массива uc_arr заполнены, возвращяем эти значения
 	uc_arr_return[0] = uc_arr[0];
 	uc_arr_return[1] = uc_arr[1];
 	uc_arr_return[2] = uc_arr[2];
@@ -39,15 +47,21 @@ void v_convert_ip(char *ch_ip_addr, unsigned char* uc_arr_return) {
     \example v_get_ip.cpp
 */
 void v_get_ip(char* ch_return) {
+	// Задаём буфер в 32 элемента
 	char buff[32];
+	// Открываем файл ip.ini для записи
 	ifstream fin("ip.ini");
+	// Если файл открыт, получаем первую строку и записываем её в буфер (buff)
 	if (fin.is_open()) {
 		fin.getline(buff, 32);
 	}
+	// Закрываем файл ip.ini
 	fin.close();
+	
     if ((buff == "localhost") || (buff == "127.0.0.1") || (buff == "0:0:0:0:0:0:0:1")) {
         char buff[32] = "localhost";
     }
+	// Возвращяем массив char
 	for(int i=0; i < 16; ++i){
 		ch_return[i] = buff[i];
 	}
