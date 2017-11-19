9e1b35cd81d3ea2e563a0a227bb85690867e98c6
Darlakon <33007083+Darlakon@users.noreply.github.com>
2017-11-19 22:53:25 +0500

    Update ADB_mod.h

--- src/core/ADB_mod.h
+++ src/core/ADB_mod.h
@@ -3,8 +3,12 @@
 \brief Заголовочный файл с подключением модуля работы с adb.
 \author Darlakon
 */
+#ifndef ADB_MOD_H
+#define ADB_MOD_H
 
 /** 
 Вызов модуля adb с командой в качестве аргумента
 */
 int adb(LPWSTR cmdArgs);
+
+#endif // ADB_MOD_H
