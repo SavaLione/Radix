37bbe9df6e806331adc20c6c0e5dc60113d02bc3
Darlakon <33007083+Darlakon@users.noreply.github.com>
2017-12-04 09:38:05 +0500

    Update ADB_slave.cpp
    Fixed dynamic path in adb_files

--- src/core/ADB_slave.cpp
+++ src/core/ADB_slave.cpp
@@ -9,8 +9,8 @@ int adb_state()
 
 int adb_files()
 {
-	adb("adb push /dl/recovery.zip sd");
-	adb("adb push /dl/su.apk sd");
+	adb("adb push -p "%CD%"\dl\recovery.zip sd");
+	adb("adb push -p "%CD%"\dl\su.zip sd");
 	return 0;
 }
 
@@ -18,7 +18,7 @@ int adb_root()
 {
 	adb("adb reboot recovery");
 	adb("adb install recovery.zip");
-	adb("adb install su.apk");
+	adb("adb install su.zip");
 	adb("adb reboot");
 	return 0;
 }
